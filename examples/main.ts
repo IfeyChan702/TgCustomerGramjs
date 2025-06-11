edf917bba5baec3bc6cfeae5723c34ba69c6fa8f
mingc <mingcym@hotmail.com>
2025-06-11 09:35:42 +0530

    forward picture

--- examples/main.ts
+++ examples/main.ts
@@ -42,20 +42,31 @@ const rl = readline.createInterface({
 
   client.addEventHandler(
     async (event) => {
-      // 判断 senderId 是否为 7711217827
-      if (event.message.senderId && event.message.senderId.valueOf() === 7711217827) {
-        console.log("收到来自 7711217827 的消息：", event.message.text);
+      // 判断 senderId 是否为 8088901247
+      if (event.message.senderId && event.message.senderId.valueOf() === 8088901247) {
+        const msgText = event.message.text || "";
+        const fromPeer = (await event.message.getInputChat())!;
+        console.log("收到来自 8088901247 的消息：", msgText);
 
-        // Forward the message to 8088901247
-        await client.forwardMessages(8088901247, {messages:event.message.id,
-          fromPeer:(await event.message.getInputChat())!
-        });
-
-        // 再发一条“hello world”
-        await client.sendMessage(7700169264, { message: "hello world" });
-
-        console.log("消息已转发到 8088901247");
-
+        if (msgText.includes("123")) {
+          await client.forwardMessages(7700169264, {
+            messages: [event.message.id], // 建议用数组
+            fromPeer: fromPeer
+          });
+          // 包含“123”，发送“成功”
+          await client.sendMessage(7700169264, { message: "成功" });
+        } else {
+          // 不包含“123”，转发并发送“失败”
+          if (!fromPeer) {
+            console.log("无法获取 fromPeer，消息未转发");
+            return;
+          }
+          await client.forwardMessages(7700169264, {
+            messages: [event.message.id], // 建议用数组
+            fromPeer: fromPeer
+          });
+          await client.sendMessage(7700169264, { message: "失败" });
+        }
       }
     },
     new NewMessage({})
@@ -63,5 +74,4 @@ const rl = readline.createInterface({
 
   // 防止进程自动退出
   setInterval(() => {}, 100000);
-  // await client.sendMessage(6612558831, { message: "Hi!mingC" });
 })();
