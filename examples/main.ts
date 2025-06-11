900129c1c014e30d95384582021c4a864e2d0038
mingc <mingcym@hotmail.com>
2025-06-11 08:50:07 +0530

    generateSessionID

--- examples/main.ts
+++ examples/main.ts
@@ -2,10 +2,11 @@ import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 import readline from "readline";
 import * as fs from "fs";
+import { NewMessage } from "telegram/events";
 
 const sessionStr = fs.readFileSync("./Session.txt", "utf8").trim();
-const apiId = 0;
-const apiHash = "";
+const apiId = 23539286;
+const apiHash = "02950d6f4ebe5564112b82243315fa59";
 const stringSession = new StringSession(sessionStr); // fill this later with the value from session.save()
 
 const rl = readline.createInterface({
@@ -35,5 +36,32 @@ const rl = readline.createInterface({
   });
   console.log("You should now be connected.");
   console.log(client.session.save()); // Save this string to avoid logging in again
-  await client.sendMessage(5860047689, { message: "Hi!辉哥" });
+
+  // const messages = await client.getMessages(7711217827, {});
+  // console.log("First Message:", messages[0].message);
+
+  client.addEventHandler(
+    async (event) => {
+      // 判断 senderId 是否为 7711217827
+      if (event.message.senderId && event.message.senderId.valueOf() === 7711217827) {
+        console.log("收到来自 7711217827 的消息：", event.message.text);
+
+        // Forward the message to 8088901247
+        await client.forwardMessages(8088901247, {messages:event.message.id,
+          fromPeer:(await event.message.getInputChat())!
+        });
+
+        // 再发一条“hello world”
+        await client.sendMessage(7700169264, { message: "hello world" });
+
+        console.log("消息已转发到 8088901247");
+
+      }
+    },
+    new NewMessage({})
+  );
+
+  // 防止进程自动退出
+  setInterval(() => {}, 100000);
+  // await client.sendMessage(6612558831, { message: "Hi!mingC" });
 })();
