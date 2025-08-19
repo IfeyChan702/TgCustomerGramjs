400dc1af5313ac8c7208b0a222567c8b9da0443a
boyo <2575133589@qq.com>
2025-08-19 13:50:09 +0530

    修改成“连接池”建立连接的方法

--- Kyber/TgMain/src/models/mysqlModel.js
+++ Kyber/TgMain/src/models/mysqlModel.js
@@ -1,20 +1,45 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL!');
-});
+module.exports = {
+  query(sql, params, callback) {
+    if (typeof params === "function") {
+      callback = params;
+      params = [];
+    }
 
-module.exports = connection;
+    if (callback && typeof callback === "function") {
+      pool.getConnection((err, connection) => {
+        if (err) return callback(err);
+
+        connection.query(sql, params, (error, results, fields) => {
+          connection.release();
+          callback(error, results, fields);
+        });
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+          if (err) return reject(err);
+
+          connection.query(sql, params, (error, results, fields) => {
+            connection.release();
+            if (error) return reject(error);
+            resolve([results, fields]);
+          });
+        });
+      });
+    }
+  },
+};
