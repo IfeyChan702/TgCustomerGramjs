14e140d5ca199951615a76e5a4c0b3f554305b39
boyo <2575133589@qq.com>
2025-09-19 12:01:22 +0530

    修改配置文件

--- Kyber/TgMain/src/index.js
+++ Kyber/TgMain/src/index.js
@@ -3,19 +3,18 @@ const dotenv = require("dotenv");
 
 const express = require("express");
 
-const env = "production";
+const env =  "production";
 const envPath = path.resolve(__dirname, `../.env.${env}`);
 dotenv.config({ path: envPath });
 
 const app = require("./app");
+
 const { bot, startBot } = require("./service/system/bot");
 const createWithdrawalsRouter = require("./routes/system/withdrawals");
-
 const PORT = 8087;
 
 app.use(express.json());
 app.use("/api", createWithdrawalsRouter(bot));
-
 app.listen(PORT, () => {
   console.log(`后端服务已启动：http://localhost:${PORT}`);
 });
